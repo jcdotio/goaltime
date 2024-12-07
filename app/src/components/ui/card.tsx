092e388eb964f7081290b3f9bf371ed54a96d68a
John Chase <john@cloudsource.io>
2024-12-06 19:26:12 -0800

    ui twack

--- app/src/components/ui/card.tsx
+++ app/src/components/ui/card.tsx
@@ -6,7 +6,7 @@ const Card = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElemen
     <div
       ref={ref}
       className={cn(
-        "rounded-lg border bg-card text-card-foreground shadow-sm",
+        "rounded-lg bg-card text-card-foreground shadow-sm",
         className
       )}
       {...props}
@@ -42,7 +42,7 @@ CardTitle.displayName = "CardTitle"
 
 const CardContent = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
   ({ className, ...props }, ref) => (
-    <div ref={ref} className={cn("p-6 pt-0", className)} {...props} />
+    <div ref={ref} className={cn("p-6 pt-0 pl-0", className)} {...props} />
   )
 )
 CardContent.displayName = "CardContent"
